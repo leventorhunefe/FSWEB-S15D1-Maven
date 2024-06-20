84908c54c60337fad2330b6073d7460dfa09aefe
Levent Orhun Efe <orhunefe93@gmail.com>
2024-06-20 17:44:20 +0300

    final

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import org.example.mobile.Contact;
+import org.example.mobile.MobilePhone;
+import org.example.models.Grocery;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+//        Grocery grocery = new Grocery();
+//        grocery.startGrocery();
+//    }
 
+        MobilePhone mobilePhone = new MobilePhone("34343434343", new ArrayList<>());
+        mobilePhone.addNewContact(new Contact("orhun", "2355644343"));
+        mobilePhone.printContacts();
     }
 }
